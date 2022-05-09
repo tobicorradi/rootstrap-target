208f6d4e022897b3f59ed13dfc19728777027c85
tobicorradi <corraditobias@gmail.com>
2022-05-09 14:32:29 -0300

    Assets folder moved inside src/, along with an index file for reducing import lines

---
+++ src/assets/index.tsx
@@ -0,0 +1,7 @@
+export { default as facebookIcon } from './facebookIcon.svg';
+export { default as hamburgerIcon } from './hamburgerIcon.svg';
+export { default as iphoneScreenshot } from './iphoneScreenshot.png';
+export { default as logo } from './logo.svg';
+export { default as playIcon } from './playIcon.svg';
+export { default as playStoreIcon } from './playStoreIcon.svg';
+export { default as twitterIcon } from './twitterIcon.svg';
