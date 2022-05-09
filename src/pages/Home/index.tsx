9dd3771656f852842a75ba73a862fa77fe3fa814
tobicorradi <corraditobias@gmail.com>
2022-05-09 14:43:12 -0300

    Home page built using new components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import {
+  Button, InputField, Logo, Paragraph, PhoneSection, Title, Subtitle,
+} from '../../components/common';
 
 function Home() {
   return (
-    <div>Home Page</div>
+    <section className="flex h-full">
+      <div className="text-center bg-white half-section">
+        <Logo />
+        <Title text="Target MVD" />
+        <Subtitle text="Find people near you & Connect" />
+        <Paragraph
+          maxWidth="sm"
+          size="xs"
+          text="Create a  target  wherever on the map, specify
+          your interest: Travel, Dating, Music, etc and start conecting
+          with others who share your interest."
+        />
+        <form className="w-full mt-8 mb-4 md:w-4/12 space-y-7">
+          <InputField label="Email" id="email" type="email" />
+          <InputField label="Password" id="password" type="password" />
+          <div>
+            <Button type="submit" text="Sign In" variant="primary" />
+            <Button type="button" text="Forgot your password?" variant="subtle" />
+          </div>
+        </form>
+        <Button type="button" text="Connect with Facebook" variant="secondary" />
+        <hr />
+        <Button type="button" text="Sign Up" variant="secondary" />
+      </div>
+      <PhoneSection />
+    </section>
   );
 }
 
