4299b33ef8c7656b920ad6e1ff1ea39c7ca366d9
tobicorradi <corraditobias@gmail.com>
2022-05-09 22:56:31 -0300

    'Input' and 'label' custom classes added

--- src/components/common/InputField/index.tsx
+++ src/components/common/InputField/index.tsx
@@ -9,8 +9,8 @@ function InputField({
 }: Props) {
   return (
     <div className="flex flex-col">
-      <label className="mb-1 font-semibold tracking-[3px] uppercase text-sm" htmlFor={id}>{label}</label>
-      <input placeholder={placeholder} className="p-3 text-sm text-center uppercase border border-target-black" id={id} type={type} />
+      <label className="label" htmlFor={id}>{label}</label>
+      <input placeholder={placeholder} className="input" id={id} type={type} />
     </div>
   );
 }
