36ac3c91ded660c26d1ff5b0f85638f6baf36f24
tobicorradi <corraditobias@gmail.com>
2022-05-12 16:30:42 -0300

    Fix min password characters

--- src/schemas/index.tsx
+++ src/schemas/index.tsx
@@ -1,14 +1,14 @@
 import * as yup from 'yup';
 
 export const signUpSchema = yup.object().shape({
-  name: yup.string().required('Name is required'),
+  username: yup.string().required('Name is required'),
   email: yup
     .string()
     .email('Please enter a valid email format')
     .required('Email is required'),
   password: yup.string()
     .required('Password is required')
-    .min(6, 'Password length should be at least 6 characters'),
+    .min(8, 'Password length should be at least 8 characters'),
   confirmPassword: yup.string()
     .required('Confirm Password is required')
     .min(4, 'Password length should be at least 4 characters')
