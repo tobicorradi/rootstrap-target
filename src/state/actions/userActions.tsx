cbb6ad5502f38f7b23e8e25dc6b45c6a3ee54988
tobicorradi <corraditobias@gmail.com>
2022-05-13 15:11:20 -0300

    Log's deleted

--- src/state/actions/userActions.tsx
+++ src/state/actions/userActions.tsx
@@ -1,15 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import userService from '../../services/userService';
+import { SignUpInputsType } from '../../types/userInputsTypes';
 
 export const signUp = createAsyncThunk(
   'user/signUp',
   async (user, thunkAPI) => {
     try {
       const { data } = await userService.signIn({ user });
-      console.log('Se hizo Sign Up', data);
       return data;
     } catch ({ response: { data } }) {
-      console.log('Error en el SignUp', data);
       return thunkAPI.rejectWithValue(data);
     }
   },
