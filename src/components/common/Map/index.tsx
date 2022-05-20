86577326a9928fe3260ba20e0f913dcf839e25ce
tobicorradi <corraditobias@gmail.com>
2022-05-20 15:07:09 -0300

    Map url added to constant folder

--- src/components/common/Map/index.tsx
+++ src/components/common/Map/index.tsx
@@ -8,7 +8,7 @@ function Map() {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={17} scrollWheelZoom>
       <TileLayer
-        url={`https://api.mapbox.com/styles/v1/tobicorradi/cl3el2qne000o15pkcygzhmbq/tiles/{z}/{x}/{y}?access_token=${envVariables.MAP_TOKEN}`}
+        url={envVariables.MAP_TOKEN}
       />
       <Marker position={[51.505, -0.09]}>
         <Popup>
