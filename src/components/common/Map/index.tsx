04d4825990c358027a24d217c75652ce2ec42817
tobicorradi <corraditobias@gmail.com>
2022-05-20 14:56:58 -0300

    Env variables added to constant file

--- src/components/common/Map/index.tsx
+++ src/components/common/Map/index.tsx
@@ -2,13 +2,13 @@ import {
   MapContainer, Marker, Popup, TileLayer,
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { envVariables } from '../../../constants/envVariables';
 
 function Map() {
-  const TOKEN = import.meta.env.VITE_MAP_ACCESS_TOKEN;
   return (
     <MapContainer center={[51.505, -0.09]} zoom={17} scrollWheelZoom>
       <TileLayer
-        url={`https://api.mapbox.com/styles/v1/tobicorradi/cl3el2qne000o15pkcygzhmbq/tiles/{z}/{x}/{y}?access_token=${TOKEN}`}
+        url={`https://api.mapbox.com/styles/v1/tobicorradi/cl3el2qne000o15pkcygzhmbq/tiles/{z}/{x}/{y}?access_token=${envVariables.MAP_TOKEN}`}
       />
       <Marker position={[51.505, -0.09]}>
         <Popup>
---
+++ src/constants/envVariables.tsx
@@ -0,0 +1,4 @@
+export const envVariables = {
+  BASE_URL: import.meta.env.VITE_API_URL,
+  MAP_TOKEN: import.meta.env.VITE_MAP_ACCESS_TOKEN,
+};
