9fb8fc47466c51807a9a2b9d9eec749db0714d12
tobicorradi <corraditobias@gmail.com>
2022-05-19 23:58:25 -0300

    Map added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
+import Map from '../../components/common/Map';
+
 function Home() {
   return (
     <section className="flex h-full">
-      Map
+      <div className="w-2/6 h-screen max-w-sm bg-white" />
+      <Map />
     </section>
   );
 }
