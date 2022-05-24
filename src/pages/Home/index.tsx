8fad678d5d63d253561762047cf6f37343957f42
tobicorradi <corraditobias@gmail.com>
2022-05-24 11:21:48 -0300

    User log out feature

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,23 @@
+import { useDispatch } from 'react-redux';
 import Map from '../../components/common/Map';
+import { userIcon } from '../../assets';
+import { Button, Paragraph } from '../../components/common';
+import { logOut } from '../../state/actions/userActions';
+import useAuthentication from '../../hooks/useAuthentication';
 
 function Home() {
+  const dispatch = useDispatch();
+  const { username } = useAuthentication();
   return (
     <section className="flex h-full">
       {/* TODO: remove this empty div and create the component containing it's own logic */}
-      <div className="w-2/6 h-screen max-w-sm bg-white" />
+      <div className="flex justify-center w-2/6 h-screen max-w-sm p-5 text-center bg-white">
+        <div>
+          <img className="mb-4" src={userIcon} alt="User profile" />
+          <Paragraph text={username} />
+          <Button onClick={() => dispatch(logOut())} variant="subtle" type="button" text="Logout" />
+        </div>
+      </div>
       <Map />
     </section>
   );
