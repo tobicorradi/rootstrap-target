a3392f6b599257a49f0cc0f4906a7be9e7b4ec52
tobicorradi <corraditobias@gmail.com>
2022-05-26 11:45:10 -0300

    Fix: env variables

--- src/components/common/Map/index.tsx
+++ src/components/common/Map/index.tsx
@@ -2,14 +2,13 @@ import {
   MapContainer, TileLayer,
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { envVariables } from '../../../constants/envVariables';
 import LocationMarker from '../LocationMarker';
 
 function Map() {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={17} scrollWheelZoom>
       <TileLayer
-        url={envVariables.MAP_TOKEN}
+        url={import.meta.env.VITE_MAP_URL}
       />
       <LocationMarker />
     </MapContainer>
--- src/constants/envVariables.tsx
+++
@@ -1,4 +0,0 @@
-export const envVariables = {
-  BASE_URL: import.meta.env.VITE_API_URL,
-  MAP_TOKEN: `https://api.mapbox.com/styles/v1/tobicorradi/cl3el2qne000o15pkcygzhmbq/draft/tiles/{z}/{x}/{y}?access_token=${import.meta.env.VITE_MAP_ACCESS_TOKEN}`,
-};
