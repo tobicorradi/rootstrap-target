7854c0b09b3dbea0fd8247652550f9244d00d4c4
tobicorradi <corraditobias@gmail.com>
2022-05-26 10:14:59 -0300

    Fix for map styling not updating correctly from API

--- src/constants/envVariables.tsx
+++ src/constants/envVariables.tsx
@@ -1,4 +1,4 @@
 export const envVariables = {
   BASE_URL: import.meta.env.VITE_API_URL,
-  MAP_TOKEN: `https://api.mapbox.com/styles/v1/tobicorradi/cl3el2qne000o15pkcygzhmbq/tiles/{z}/{x}/{y}?access_token=${import.meta.env.VITE_MAP_ACCESS_TOKEN}`,
+  MAP_TOKEN: `https://api.mapbox.com/styles/v1/tobicorradi/cl3el2qne000o15pkcygzhmbq/draft/tiles/{z}/{x}/{y}?access_token=${import.meta.env.VITE_MAP_ACCESS_TOKEN}`,
 };
