0181563fdfbe0a39119977688e32eea52887fd61
tobicorradi <corraditobias@gmail.com>
2022-05-26 01:44:38 -0300

    Code readability improved

--- src/components/common/LocationMarker/index.tsx
+++ src/components/common/LocationMarker/index.tsx
@@ -4,18 +4,20 @@ import customLocationIcon from '../../../constants/customLocationIcon';
 
 function LocationMarker() {
   const map = useMap();
-  const [position, setPosition] = useState([0, 0]);
+  const [position, setPosition] = useState({ lat: 0, lng: 0 });
 
   useEffect(() => {
     map.locate().on('locationfound', (e) => {
-      setPosition([e.latlng.lat, e.latlng.lng]);
-      map.flyTo(e.latlng, map.getZoom(), { animate: false });
+      const { lat } = e.latlng;
+      const { lng } = e.latlng;
+      setPosition({ lat, lng });
+      map.flyTo({ lat, lng }, map.getZoom(), { animate: false });
     });
   }, []);
 
-  return position === null ? null : (
-    <Marker position={position} icon={customLocationIcon} />
-  );
+  if (!position) return null;
+
+  return <Marker position={position} icon={customLocationIcon} />;
 }
 
 export default LocationMarker;
