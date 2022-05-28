61c3b0c94fe28778af54f88dd480ec70036f82a9
tobicorradi <corraditobias@gmail.com>
2022-05-28 02:23:58 -0300

    Toolkit's types added

--- src/state/store/index.ts
+++ src/state/store/index.ts
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userSlice } from '../reducers/userReducer';
 
+export type RootState = ReturnType<typeof store.getState>
+
 export type AppDispatch = typeof store.dispatch
 
 export const store = configureStore({
