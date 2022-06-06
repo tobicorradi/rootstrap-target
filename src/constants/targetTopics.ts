3f8b65ad860b54c585d15f1968dc4fedf1bced0e
tobicorradi <corraditobias@gmail.com>
2022-06-06 12:17:49 -0300

    Todo comment added

--- src/constants/targetTopics.ts
+++ src/constants/targetTopics.ts
@@ -4,7 +4,7 @@ interface Topic {
     text: string;
     value: string;
 }
-
+// TODO: Replace with Topics from API
 const TargetTopics: Array<Topic> = [
   {
     id: 1,
@@ -12,49 +12,6 @@ const TargetTopics: Array<Topic> = [
     text: 'Football',
     value: 'football',
   },
-  {
-    id: 2,
-    icon: 'test.svg',
-    text: 'Travel',
-    value: 'travel',
-  },
-  {
-    id: 3,
-    icon: 'test.svg',
-    text: 'Politics',
-    value: 'politics',
-  },
-  {
-    id: 4,
-    icon: 'test.svg',
-    text: 'Art',
-    value: 'art',
-  },
-  {
-    id: 5,
-    icon: 'test.svg',
-    text: 'Music',
-    value: 'music',
-  },
-  {
-    id: 6,
-    icon: 'test.svg',
-    text: 'Movies',
-    value: 'movies',
-  },
-  {
-    id: 7,
-    icon: 'test.svg',
-    text: 'Series',
-    value: 'series',
-  },
-  {
-    id: 8,
-    icon: 'test.svg',
-    text: 'Food',
-    value: 'food',
-  },
-
 ];
 
 export default TargetTopics;
